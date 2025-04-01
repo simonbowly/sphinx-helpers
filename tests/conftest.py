ee8fc9428efbd89afc525737e54529559ce6594e
Simon Bowly <simon.bowly@gmail.com>
2025-04-01 12:41:23 +1100

    Add difflib explanation for string comparisons

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
+import difflib
+
 pytest_plugins = ("sphinx.testing.fixtures",)
+
+def pytest_assertrepr_compare(op, left, right):
+    if isinstance(left, str) and isinstance(right, str) and op == "==":
+        return list(difflib.context_diff(left.split("\n"), right.split("\n")))
